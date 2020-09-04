8eafa9e1d8ca5b12ea84b5d738ad69db4b507146
Nneka Nnagbo <nnekakassandra@gmail.com>
2020-09-03 20:52:58 -0400

    Add: logging previous attempts

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 let computerGuess;
 let userGuesses = [];
+let attempts = 0;
 
 function init() {
     computerGuess = Math.floor(Math.random() * 100 + 1);
-    console.log(computerGuess);
     document.getElementById("newGameButton").style.display = "none";
     document.getElementById("gameArea").style.display = "none";
 }
@@ -25,7 +25,8 @@ function compareGuess() {
     const userGuess = Number(document.getElementById("inputBox").value);
     userGuesses.push(" " + userGuess);
     document.getElementById("guesses").innerHTML = userGuesses;
-
+    attempts++;
+    document.getElementById("attempts").innerHTML = attempts;
     if(userGuess > computerGuess) {
         document.getElementById("textOutput").innerHTML = "Your guess is too high";
         document.getElementById("inputBox").value = "";
@@ -33,7 +34,8 @@ function compareGuess() {
         document.getElementById("textOutput").innerHTML = "Your guess is too low";
         document.getElementById("inputBox").value = " ";
     } else {
-        document.getElementById("textOutput").innerHTML = "Correct!";
+        document.getElementById("textOutput").innerHTML = 
+        "Correct! You got it in " + attempts + " attempts";
     }
 
 }
